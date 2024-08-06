c282fd565266b092fb2033e1358a4bea773b02c6
famgz <famgz@proton.me>
2024-08-06 20:09:27 -0300

    chore: update shadcn theme and global color variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <Button>Hello World</Button>;
 }
